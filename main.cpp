9c6e0da1253ca5fa2cb322e154bc17d8cda7e01a
nedogeek <urunbaev.timur@gmail.com>
2023-12-05 12:33:00 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define MYPORT 8000
+#define BACKLOG 10
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
